bd9eceb123890fae3afe88d53844a502976c0881
Mohit Patil <mohitpatil546@gmail.com>
2018-05-07 15:48:00 +0530

    - bug resolved

--- src/app/components/weather-search/weather-search.component.ts
+++ src/app/components/weather-search/weather-search.component.ts
@@ -3,26 +3,27 @@ import { WeatherService } from '../../services/weather.service';
 import { WeatherItem } from './../../interfaces/weather-item';
 
 @Component({
-  selector: 'app-weather-search',
-  templateUrl: './weather-search.component.html',
-  styleUrls: ['./weather-search.component.css']
+	selector: 'app-weather-search',
+	templateUrl: './weather-search.component.html',
+	styleUrls: ['./weather-search.component.css']
 })
 export class WeatherSearchComponent implements OnInit {
-    city: string = '';
-  constructor(private weatherService: WeatherService) { }
+	city: string = '';
+	constructor(private weatherService: WeatherService) { }
 
-  ngOnInit() {
-  }
-  onSubmit(){
-    this.weatherService.searchWeatherData(this.city).subscribe(
-      data => {
-        const weatherItem: WeatherItem = {
-          'cityName': data.name,
-          'description': data.weather[0].description,
-          'temperature': data.main.temp       
-        }
-      }
-    )
-  }
+	ngOnInit() {
+
+	}
+	onSubmit(){
+		this.weatherService.searchWeatherData(this.city).subscribe(
+			data => {
+				const weatherItem: WeatherItem = {
+					'cityName': data['name'],
+					'description': data['weather'][0].description,
+					'temperature': data['main'].temp
+				}
+			}
+		)
+	}
 
 }
