e374d46dd090a5347a4dd01b26a8b2188a230c02
abhi747 <abhi.kk747@gmail.com>
2019-07-23 14:52:31 +0530

    for deployment

---
+++ dist/assets/data/weather.data.ts
@@ -0,0 +1,7 @@
+import { WeatherItem } from './../../app/interfaces/weather-item';
+
+export const WEATHER_ITEMS: WeatherItem[] = [
+    {cityName:'London', description:'Rainy', temperature:7},
+    {cityName:'NY', description:'Cloudy', temperature:12},
+    {cityName:'Mumbai', description:'Sunny', temperature:24}
+];
